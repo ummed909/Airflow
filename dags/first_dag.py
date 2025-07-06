12a66cd16ee57f2c7608f9ee3171cbeb24ae406d
ummed choudhary <ummedc184@gmail.com>
2025-07-06 18:47:54 +0530

    ETL Pipeline for practice purpose- ummed

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -3,10 +3,24 @@ from airflow.decorators import python_task
 from airflow.example_dags.example_latest_only import task1
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+import requests
+
+source_url = "https://jsonplaceholder.typicode.com/todos/1"
+
 
 def demo_func():
     print('Hello World, I am Ummed')
     print('First Pipeline')
+    print('-----------------------------------------')
+    try:
+        response = requests.get(source_url)
+        data = response.json()
+        print(f"Fetched {len(data)} items from API.")
+        print(type(data))
+    except Exception as e:
+        print(f"Error during extraction: {e}")
+        raise
+    print('---------------------------------------')
 
 with DAG(
     dag_id='first_dag',
